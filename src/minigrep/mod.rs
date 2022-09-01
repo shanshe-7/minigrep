81552627c2191f96087da0454d129f1867ab43d8
shanshe-7 <shanshe1993@gmail.com>
2022-09-01 12:11:33 +0400

    feat: remove clone and implement iterator

--- src/minigrep/mod.rs
+++ src/minigrep/mod.rs
@@ -7,7 +7,7 @@ use std::error::Error;
 pub fn start() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(| err | {
+    let config = Config::new(env::args()).unwrap_or_else(| err | {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -28,12 +28,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename"),
+        };
 
         let case_sensitive = env::var("CASE_SENSITIVE").is_err();
     
@@ -63,14 +68,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 } 
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec:: new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
